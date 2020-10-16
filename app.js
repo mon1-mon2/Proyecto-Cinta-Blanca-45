5f69657c91940a8c3478db0e9b24e49f45c445f9
non1-mon2 <cardenas_ramon@hotmail.com>
2020-10-16 13:44:53 -0500

    ultima version

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+console.log ("Hola desde Priyecto Cinta Blanca 45 !")
+
 // declarar los inputs
 const nombre = document.getElementById ("nombre");
 const direccion = document.getElementById ("direccion");
 const telefono = document.getElementById ("telefono");
-const edad = document.getElementById ("edad");
-const distancia = document.getElementById ("distancia");
+const sexo = document.getElementById ("sexo");
+const categoria = document.getElementById ("categoria");
 const boton = document.getElementById ("boton");
 
     // RESPUESTAS
 const respuestaNnombre = document.getElementById ("respuestaNombre");
 const respuestaDireccion = document.getElementById ("respuestaDireccion");
 const respuestaTelefono = document.getElementById ("respuestaTelefono");
-const respuestaEdad = document.getElementById ("respuestaEdad");
+const respuestaSexo = document.getElementById ("respuestaSexo");
 const socio = document.getElementById ("socio");
 const progress = document.getElementById ("progress");
 const imagen = document.getElementById('imagen');
@@ -18,40 +20,37 @@ const imagen = document.getElementById('imagen');
 const card = document.getElementById('card');
 card.style.display ="none";
 
-// Paso 2: Declarar la función que generará la card con las entradas del usuario
+const serSocio = () => {
 
-const generarOrden = () => {
-
-//  . . .validar que ningún campo esté vacío
-  if ( nombre.value === "" || direccion.value === "" || telefono.value === "" || edad.value === "" ) {
+  if ( nombre.value === "" || direccion.value === "" || telefono.value === "" || sexo.value === "" || categoria.value === "" ) {
     alert (" Debes ingresar valores en los campos")
   } else {
-    // Las respuestas cambien por lo ingresado por los usuarios en los INPUTS
+
   respuestaNombre.innerHTML = nombre.value;
   respuestaDireccion.innerHTML = direccion.value;
   respuestaTelefono.innerHTML = telefono.value;
-  respuestaEdad.innerHTML = edad.value;
+  socio.innerHTML = categoria.value;
 
+  const valorCategoria = categoria.value.toLowerCase();
 
-  if(valorPina === "con piña") {
-    pedido.innerHTML = "Pizza CON piña";
-    imagen.src = "https://i.blogs.es/58a778/pizza-hawaiana3/840_560.jpg"
-    progress.style.width = "25%";
-  } else if (pina.value === "sin piña") {
-    pedido.innerHTML = "Pizza SIN piña";
-    imagen.src ="https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg"
-    progress.style.width = "25%";
-  } else {
-    pedido.innerHTML = "El usuario no indicó su pedido";
-    imagen.src = "https://cdn.cheapism.com/images/091417_restaurant_mistakes_slide_11_fs.max-420x243.jpg"
+  if(valorCategoria === "principiante") {
+    socio.innerHTML = " ¡ Es momento de empezar a correr ! ";
+    imagen.src = "https://www.runnea.com/archivos/201705/correr-despues-de-parto-838x400x80xX.jpg?1"
+  } else if (categoria.value === "10 km") {
+    socio.innerHTML = "¡ tus primeros 10 km !";
+    imagen.src ="https://thumbs.dreamstime.com/b/km-medium-running-distance-splash-paint-sign-km-medium-running-distance-124970072.jpg"
+  } else if (categoria.value === "21 km") {
+    socio.innerHTML = "¡ disfruta y mejora tu tiempo en los 21 km !";
+    imagen.src ="https://marcosedo.com/wp-content/uploads/2014/01/experiencia_21k.jpg"
+  } else if (categoria.value === "Maratón") {
+    socio.innerHTML = "¡ Mejora el tiempo en tus 42 km !";
+    imagen.src ="42 km.jpg"
   }
 
   card.style.display ="";
 
-  console.log("Orden generada");
+  console.log("Solicitud Aceptada");
   }
 }
 
-// Paso 3: Agregar el event listener del botón para que ejecuté la función que genere la card
-
-boton.addEventListener("click", generarOrden);
+boton.addEventListener("click", serSocio);
